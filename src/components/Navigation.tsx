2e29bbae8de49848dd9896e4075dee9fb913da34
smarthulala <chao.long.au@gmail.com>
2023-08-15 00:23:02 +1000

    init

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,29 @@
 'use client'
-// import { useState } from 'react'
-// import { RiMoonFill, RiSunLine } from 'react-icons/ri'
-// import { useTheme } from 'next-themes'
-// import { IoMdMenu, IoMdClose } from 'react-icons/io'
+import { useState } from 'react'
+import { RiMoonFill, RiSunLine } from 'react-icons/ri'
+import { useTheme } from 'next-themes'
+import { IoMdMenu, IoMdClose } from 'react-icons/io'
 
-// interface NavItem {
-//   label: string
-//   page: string
-// }
+interface NavItem {
+  label: string
+  page: string
+}
 
-// const NavItems: Array<NavItem> = [
-//   { label: 'Home', page: 'home' },
-//   { label: 'About', page: 'about' },
-//   { label: 'Projects', page: 'projects' },
-// ]
+const NavItems: Array<NavItem> = [
+  { label: 'Home', page: 'home' },
+  { label: 'About', page: 'about' },
+  { label: 'Projects', page: 'projects' },
+]
 
 function Navbar() {
-  // const { systemTheme, theme, setTheme } = useTheme()
-  // const currentTheme = theme === 'system' ? systemTheme : theme
-  // console.log(theme)
-  // const [navbar, setNavbar] = useState(false)
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === 'system' ? systemTheme : theme
+  console.log(theme)
+  const [navbar, setNavbar] = useState(false)
 
   return (
     <header className='w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600'>
-      {/* <div className='justify-between md:items-center md:flex'>
+      <div className='justify-between md:items-center md:flex'>
         <div>
           <div className='md:py-5 md:block'>
             <h2 className='test-2xl font-bold dark:text-white'>Chao, Long</h2>
@@ -41,7 +41,7 @@ function Navbar() {
             {currentTheme === 'dark' ? <RiSunLine /> : <RiMoonFill />}
           </button>
         </div>
-      </div> */}
+      </div>
     </header>
   )
 }
