844b26550a7ce5e065a2869888a212566d9fce21
smarthulala <chao.long.au@gmail.com>
2023-08-16 10:56:00 +1000

    refactor

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,12 +5,25 @@ import { BsGithub, BsArrowUpRightSquare } from 'react-icons/bs'
 
 const projects = [
   {
-    name: 'Thankful Thoughts',
+    name: 'CrankBit, Automotive SaaS Platform',
     description:
       'Boosting automotive mechanics efficiency, this website aids in creating streamlined repair forms, optimizing processes for enhanced workflow, and overall productivity improvement.',
     image: '/thankfulthoughts.png',
     github: 'https://github.com/CRANKBIT',
     link: 'https://thankfulthoughts.io/',
+    stack: [
+      { title: 'Agile development environment' },
+      { title: 'problem-solving' },
+      { title: 'Used Redux Toolkit Query to fetch data' },
+      { title: 'Collaborative web page UI design' },
+      { title: 'React, Tailwind and TypeScript' },
+      { title: 'MongoDb, Mongoose' },
+      { title: 'Created the database schema, model, controller' },
+      { title: 'Reviewd and made changes upon pull requests on git' },
+      { title: 'Jira and GitHub' },
+      { title: '' },
+      { title: '' },
+    ],
   },
   {
     name: 'PlatoIO',
@@ -29,57 +42,60 @@ const projects = [
   },
 ]
 
-const ProjectsSection = () => {
+const Projects = () => {
   return (
-    <section id='projects'>
-      <h1 className='my-10 text-center font-bold text-4xl'>
-        Projects
-        <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
-      </h1>
-
+    <div className='px-60 mb-20'>
+      <div className='my-10 text-center font-bold text-6xl'>Projects</div>
       <div className='flex flex-col space-y-28'>
         {projects.map((project, idx) => {
           return (
-            <div key={idx}>
-              <div className='flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
-                <div className=' md:w-1/2'>
-                  <Link href={project.link}>
-                    <Image
-                      src={project.image}
-                      alt=''
-                      width={1000}
-                      height={1000}
-                      className='rounded-xl shadow-xl hover:opacity-70'
+            <div key={idx} className='flex flex-col md:flex-row md:space-x-12'>
+              <div className='md:w-1/2'>
+                <Link href={project.link}>
+                  <Image
+                    src={project.image}
+                    alt=''
+                    width={1000}
+                    height={1000}
+                    className='rounded-xl shadow-xl dark:md:shadow-amber-500/50 hover:opacity-70'
+                  />
+                </Link>
+              </div>
+              <div className='mt-8 md:w-1/2'>
+                <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
+                <p className='text-xl leading-7 mb-4  dark:text-neutral-400'>
+                  {project.description}
+                </p>
+                <div className='flex gap-4 py-4'>
+                  <Link href={project.github} target='_blank'>
+                    <BsGithub
+                      size={30}
+                      className='hover:-translate-y-1 transition-transform cursor-pointer'
+                    />
+                  </Link>
+                  <Link href={project.link} target='_blank'>
+                    <BsArrowUpRightSquare
+                      size={30}
+                      className='hover:-translate-y-1 transition-transform cursor-pointer'
                     />
                   </Link>
                 </div>
-                <div className='mt-8 md:w-1/2'>
-                  <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
-                  <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400'>
-                    {project.description}
-                  </p>
-                  <div className='flex flex-row align-bottom space-x-4'>
-                    <Link href={project.github} target='_blank'>
-                      <BsGithub
-                        size={30}
-                        className='hover:-translate-y-1 transition-transform cursor-pointer'
-                      />
-                    </Link>
-                    <Link href={project.link} target='_blank'>
-                      <BsArrowUpRightSquare
-                        size={30}
-                        className='hover:-translate-y-1 transition-transform cursor-pointer'
-                      />
-                    </Link>
-                  </div>
+                <div>
+                  Highlights:
+                  {project.stack &&
+                    project.stack.map((item, index) => (
+                      <ul key={index} className='text-sm my-2'>
+                        {item.title}
+                      </ul>
+                    ))}
                 </div>
               </div>
             </div>
           )
         })}
       </div>
-    </section>
+    </div>
   )
 }
 
-export default ProjectsSection
+export default Projects
